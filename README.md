bd3a1328e85651a24f80d4989ef198817455650c
DAIXK <877846071@qq.com>
2016-04-25 23:47:09 +0800

    third

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 this is a boring git.
 
 www.cnblogs.com/zhangsf/archive/2013/06/13/3134409.html
+数组告一段落。 4/25/2016
--- task.c
+++ task.c
@@ -13,7 +13,7 @@ void task2()
 	}
 	int min = 0;
 	int temp;
-	for(j= 0;j<99;j++)  //假如min下标为最小，所以就剩下99个数组 0-99 为一百个下标
+	for(j= 0;j<100;j++)  //假如min下标为最小，所以就剩下99个数组 0-99 为一百个下标
 	{
 		min= j;
 		for(k = j+1;k<100;k++) //然后在100个下标里面比较
@@ -34,39 +34,88 @@ void task2()
 		
 		
 		}
-		printf("%d\n",num[j]);
+		printf("%-4d",num[j]);
 	}
 
 }
-int  main()
+void  task1()
 {
 	int a[10];
-	int i,j,k;
+	int i,j,h,k;
 
-	for(i = 0;i<10;i++)
+	for(h = 0;h<10;h++)
 	{
-		scanf("%d",&a[i]);
-	
-	
+		scanf("%d",&a[h]);
 	}
+	int min= 0;
+	int temp;
 	for(i = 0;i <10;i++)
 	{
-	 printf("%d\n",a[i]);
-
+		min = i;
+		for(j=i+1;j<10;j++)
+		{
+			if(a[j]>a[min])
+			{
+				min =j;
+			}
+		}
+		if(min != i)
+		{
+			temp= a[i];
+			a[i] = a[min];
+			a[min]=temp;
+		
+		
+	
+		}
+	}
+	for(k=9;k>=0;k--)
+	{
+		printf("%-4d",a[k]);
+	
+	}
+	printf("\n");
+	for(k=0;k<10;k++)
+	{
+	
+		printf("%-4d",a[k]);
 	}
 
-	return 0;
+	printf("\n");
 }
 
 
-void main1()
+void task3()
+{
+	int a[3][4];
+	int i;
+	for(i = 1; i < 13; i++)
+	{
+		a[i/4][i%4] = i;  //二维数组一次循环算法为a[k][j]   a[i/j][i%j] 以第二个坐标为准。
+	
+		printf("%-4d",a[i/4][i%4]);
+	}
+
+
+
+
+
+
+
+}
+
+
+
+
+void main()
 {
 	
 	printf("习题一：输入10个数，存入一个数组，并输出从小到大，从大到小\n");
+	task1();
 	printf("习题二：随机生成100个数，实现排序\n");
 	task2();
 	printf("习题三：实现循环一次初始化二维数组\n");
-
+	task3();
 
 
 
